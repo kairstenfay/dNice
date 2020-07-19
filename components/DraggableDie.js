0763466d26d6638a1fdeb2b5d55e6f8f4fdd5566
Kairsten Fay <kairsten.fay@gmail.com>
2020-07-19 12:52:54 -0700

    Spin die on release

--- components/DraggableDie.js
+++ components/DraggableDie.js
@@ -9,6 +9,30 @@ import Die from './Die'
 
 export default function DraggableDie(props) {
   const pan = useRef(new Animated.ValueXY()).current
+  const twirl = useRef(new Animated.Value(0)).current
+
+  const startSpring = () => (
+    Animated.spring(pan, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start(({ finished }) => props.onRelease())
+  )
+
+  const startTwirl = () => (
+    Animated.timing(twirl, {
+      toValue: 360,
+    }).start(({ finished }) => twirl.resetAnimation())
+  )
+
+
+  const rotateInterpolate = twirl.interpolate({
+    inputRange: [0, 360],
+    outputRange: ["0deg", "360deg"],
+  })
+
+  const animatedStyles = {
+    transform: [{rotate: rotateInterpolate }]
+  }
 
   const panResponder = useRef(
     PanResponder.create({
@@ -21,20 +45,17 @@ export default function DraggableDie(props) {
         },
       ]),
       onPanResponderRelease: () => {
-        Animated.spring(pan,
-          {
-            toValue: { x: 0, y: 0 },
-            useNativeDriver: false,
-          }).start(
-            ({ finished }) => props.onRelease()
-        )
+        Animated.sequence([
+          startSpring(),
+          startTwirl()
+        ])
       }
     })
   ).current
 
   return (
     <Animated.View
-        style={[pan.getLayout()]}
+        style={[pan.getLayout(), animatedStyles]}
         {...panResponder.panHandlers}
         >
           <View style={styles.dieContainer}>
