afdbe6048fa43d1292761b3995e4d332a2d109d3
Kairsten Fay <kairsten.fay@gmail.com>
2020-07-18 22:54:08 -0700

    Edit styles

--- components/styles.js
+++ components/styles.js
@@ -2,6 +2,11 @@ import { StyleSheet } from 'react-native';
 
 
 const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    paddingVertical: 20,
+  },
+
   container: {
     flex: 1,
     justifyContent: 'center',
