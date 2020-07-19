74cac0330cab296f79f6e77a83fc518566136e45
Kairsten Fay <kairsten.fay@gmail.com>
2020-07-18 22:36:02 -0700

    UserControls: Add dice bag

--- components/UserControls.js
+++ components/UserControls.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import {
+    Image,
     Text,
     TextInput,
     View,
 } from 'react-native'
 import { styles } from './styles'
+
+
+const DiceBag = () => {
+    // Per React docs, Image paths must be defined statically
+    const icons = {
+        4: require('../img/noun_d4.png'),
+        6: require('../img/noun_d6.png'),
+        8: require('../img/noun_d8.png'),
+        10: require('../img/noun_d10.png'),
+        12: require('../img/noun_d12.png'),
+        20: require('../img/noun_d20.png'),
+    }
+
+    return (
+        <View>
+            { Object.keys(icons).map(d =>
+                <Image key={d}
+                    source={icons[d]}
+                    style={styles.miniDieImage}
+                    />
+            )}
+        </View>
+    )
+}
 
 export default function UserControls(props) {
     return (
@@ -25,6 +50,7 @@ export default function UserControls(props) {
             keyboardType='number-pad'
             />
         </View>
+        <DiceBag />
       </>
     )
   }
