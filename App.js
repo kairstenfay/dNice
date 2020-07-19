4ded69efa97920eb84d21fe6f281df1dd5b6f064
Kairsten Fay <kairsten.fay@gmail.com>
2020-07-18 20:33:08 -0700

    Refactor user controls to new component

--- App.js
+++ App.js
@@ -43,6 +43,28 @@ function Die(props) {
   )
 }
 
+const UserControls = (props) => {
+  return (
+    <>
+      <View style={styles.dimensionContainer}>
+        <Text style={styles.displayText}>
+          Rolling d{props.dimensions}
+        </Text>
+      </View>
+      <View>
+        <Text>
+          Modify:
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={text => props.setDimensions(text)}
+          defaultValue={props.dimensions.toString()}
+          keyboardType='number-pad'
+          />
+      </View>
+    </>
+  )
+}
 
 function Roll() {
   const [dimensions, setDimensions] = useState('20')
@@ -68,25 +90,9 @@ function Roll() {
     setResult(Math.floor(Math.random() * dimensions) + 1)
   }
 
-
   return (
     <View>
-      <View style={styles.dimensionContainer}>
-        <Text style={styles.displayText}>
-          Rolling d{dimensions}
-        </Text>
-        <View>
-          <Text>
-            Modify:
-          </Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setDimensions(text)}
-            defaultValue={dimensions.toString()}
-            keyboardType='number-pad'
-            />
-        </View>
-      </View>
+      <UserControls dimensions={dimensions} setDimensions={setDimensions} />
       <Animated.View
         style={{
           transform: [{ translateX: pan.x }, { translateY: pan.y }]
